f026c1c99114d3b228d0b2c187adfdf3654d7221
saniketprdxn <anikets@prdxn.com>
2021-02-10 12:44:45 +0530

    added news api to our eleventy static site to make it dynamic

---
+++ src/_data/news.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = async function() {
+    try {
+        url = `http://newsapi.org/v2/everything?q=tesla&from=2021-01-10&sortBy=publishedAt&apiKey=${process.env.API_KEY}`;
+        const article = await axios.get(url);
+        console.log(article.data);
+        return article.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
