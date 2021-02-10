5bf8f52af69aa54b214f0f16f754be902c645b67
saniketprdxn <anikets@prdxn.com>
2021-02-10 12:51:28 +0530

    added news api key descriptive name

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function() {
     try {
-        url = `http://newsapi.org/v2/everything?q=tesla&from=2021-01-10&sortBy=publishedAt&apiKey=${process.env.API_KEY}`;
+        url = `http://newsapi.org/v2/everything?q=tesla&from=2021-01-10&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`;
         const article = await axios.get(url);
         console.log(article.data);
         return article.data;
